acad320b8879afe010a45a3c6cafff16e75033cf
ISNing <33572057+ISNing@users.noreply.github.com>
2023-01-16 11:46:22 +0800

    curve_fitting.py: remove unused parameter

--- Android/tree_making/curve_fitting.py
+++ Android/tree_making/curve_fitting.py
@@ -1,5 +1,5 @@
 # 用于拟合生成详细的背光曲线数据
-# 使用fitted_function(x, step_size, start, stop)在指定步长，起始值和终止值之间生成函数值
+# 使用fitted_function(step_size, start, stop)在指定步长，起始值和终止值之间生成函数值
 from scipy.optimize import curve_fit
 
 # Define the function to be fitted
@@ -14,6 +14,6 @@ y_data = [1, 2, 3, 2, 3, 4]
 params, cov = curve_fit(func, x_data, y_data)
 
 # Generate the fitted function
-def fitted_function(x, step_size=0.1, start=0, stop=10):
+def fitted_function(step_size=0.1, start=0, stop=10):
     x_values = np.arange(start, stop + step_size, step_size)
     return func(x_values, *params)
